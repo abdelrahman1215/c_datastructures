d933599f8d464219315f8c038f87eb826b7d0ab4
abdelrahman1215 <abdelrahmansoliman968@gmail.com>
2024-02-04 20:49:28 +0200

    made some changes to the test bash script

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
-make data_tests
+/mnt/c/ProgramData/chocolatey/bin/make.exe data_tests
 clear
-printf "hashmap tests :\n" ; ./HashMapTest.exe 
+printf 'hashmap tests :\n' ; ./HashMapTest.exe 
 printf '\nlinked list tests :\n' ; ./LinkedListTest.exe
 printf '\nc_vector tests :\n' ; ./C_vectorTest.exe
